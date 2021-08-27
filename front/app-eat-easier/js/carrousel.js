8f0e7dfbf66cd0bdb5bbe158881bd5b26ae09104
RosaEugenia <rosa.eugenia.ortega@gmail.com>
2021-08-26 23:18:48 -0500

    carrousel actualizacion de menu hamburguesa

---
+++ front/app-eat-easier/js/carrousel.js
@@ -0,0 +1,53 @@
+new Glide(".images", {
+    type: 'carousel',
+    perView: 3,
+    focusAt: 'center',
+    gap: 30,
+    breakpoints: {
+        1200: {
+            perView: 3
+        },
+        768: {
+            perView: 2
+        },
+        576: {
+            perView: 1
+        }
+    }
+}).mount();
+
+new Glide(".images-1", {
+    type: 'carousel',
+    perView: 3,
+    focusAt: 'center',
+    gap: 30,
+    breakpoints: {
+        1200: {
+            perView: 3
+        },
+        768: {
+            perView: 2
+        },
+        576: {
+            perView: 1
+        }
+    }
+}).mount();
+
+new Glide(".images-2", {
+    type: 'carousel',
+    perView: 3,
+    focusAt: 'center',
+    gap: 30,
+    breakpoints: {
+        1200: {
+            perView: 3
+        },
+        768: {
+            perView: 2
+        },
+        576: {
+            perView: 1
+        }
+    }
+}).mount();
