cc1db478b1bbb47826742d4c4ba50813a4d13f87
RosaEugenia <rosa.eugenia.ortega@gmail.com>
2021-09-04 13:27:45 -0500

    food preferences front done and add icons

---
+++ front/app-eat-easier/js/botones_food.js
@@ -0,0 +1,129 @@
+console.log("hola")
+
+let meat = document.querySelector('#meat')
+let chicken = document.querySelector('#chicken')
+let pork = document.querySelector('#pork')
+let fish = document.querySelector('#fish')
+let egg = document.querySelector('#egg')
+let cheese = document.querySelector('#cheese')
+let fruit = document.querySelector('#fruit')
+let vegetables = document.querySelector('#vegetables')
+let wheat = document.querySelector('#wheat')
+
+meat.addEventListener('click', (e) => {
+    console.log(e.target.classList)
+
+    if (e.target.classList.contains('check')) {
+
+        e.target.classList.remove('check')
+
+    } else {
+        e.target.classList.add('check')
+    }
+    // e.target.classList
+})
+
+
+chicken.addEventListener('click', (e) => {
+    console.log(e.target.classList)
+
+    if (e.target.classList.contains('check')) {
+
+        e.target.classList.remove('check')
+
+    } else {
+        e.target.classList.add('check')
+    }
+    // e.target.classList
+})
+
+pork.addEventListener('click', (e) => {
+    console.log(e.target.classList)
+
+    if (e.target.classList.contains('check')) {
+
+        e.target.classList.remove('check')
+
+    } else {
+        e.target.classList.add('check')
+    }
+    // e.target.classList
+})
+
+fish.addEventListener('click', (e) => {
+    console.log(e.target.classList)
+
+    if (e.target.classList.contains('check')) {
+
+        e.target.classList.remove('check')
+
+    } else {
+        e.target.classList.add('check')
+    }
+    // e.target.classList
+})
+
+egg.addEventListener('click', (e) => {
+    console.log(e.target.classList)
+
+    if (e.target.classList.contains('check')) {
+
+        e.target.classList.remove('check')
+
+    } else {
+        e.target.classList.add('check')
+    }
+    // e.target.classList
+})
+
+cheese.addEventListener('click', (e) => {
+    console.log(e.target.classList)
+
+    if (e.target.classList.contains('check')) {
+
+        e.target.classList.remove('check')
+
+    } else {
+        e.target.classList.add('check')
+    }
+    // e.target.classList
+})
+
+fruit.addEventListener('click', (e) => {
+    console.log(e.target.classList)
+
+    if (e.target.classList.contains('check')) {
+
+        e.target.classList.remove('check')
+
+    } else {
+        e.target.classList.add('check')
+    }
+    // e.target.classList
+})
+
+vegetables.addEventListener('click', (e) => {
+    console.log(e.target.classList)
+
+    if (e.target.classList.contains('check')) {
+
+        e.target.classList.remove('check')
+
+    } else {
+        e.target.classList.add('check')
+    }
+    // e.target.classList
+})
+
+wheat.addEventListener('click', (e) => {
+    console.log(e.target.classList)
+
+    if (e.target.classList.contains('check')) {
+
+        e.target.classList.remove('check')
+
+    } else {
+        e.target.classList.add('check')
+    }
+    // e.target.classList
+})
